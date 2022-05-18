3285628148b33393ba3cf6ec04ceb3b8387b43d9
satyam kakra <satyamkakra4u@gmail.com>
2022-05-19 00:13:13 +0530

    movies title

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,33 +1,37 @@
-import React, { Component } from 'react'
-import {movies} from './MovieData'
+import React, { Component } from 'react';
+import { movies } from './MovieData';
 
 
 export class MovieList extends Component {
   render() {
-       let moviesArr = movies.results
-    
+    let moviesArr = movies.results;
+
     return (
         <>
-      <div>
-          
+        <div>
+
           <h3 className='text-center'><strong>Trending</strong></h3>
 
-      </div>
-      <div className='movies-list'>
-          {
-              moviesArr.map((movieElem)=>(
-                <div className="card movie-card">
-              <img src="https://collider.com/wp-content/uploads/the-avengers-movie-poster-banners-03.jpg" style={{height:'40vh' , width:'20vw'}} className="card-img-top movie-img" alt="..."/>
-                <h5 className="card-title banner-title">{movieElem.title}</h5>
-               
-            </div>
-              ))
-              
-          }
+        </div>
+        <div className='movies-list'>
+          {moviesArr.map((movieElem) => (
+            <div className="card movie-card">
+              <img src="https://collider.com/wp-content/uploads/the-avengers-movie-poster-banners-03.jpg" style={{ height: '40vh', width: '20vw' }} className="card-img-top movie-img" alt="..." />
 
-      </div>
-      </>
-    )
+              <h5 className="card-title movie-title">{movieElem.title}</h5>
+
+              <div className="button-wrapper" style={{ display: "flex", justifyContent: "center" }}/>
+                <a href="#" className="btn btn-primary movies-button text-center">Add to Favourites</a>
+                <div />
+
+              </div>
+  ))}
+
+            </div>
+
+        </>
+
+        );
   }
 }
 
