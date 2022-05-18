8861684c6b3ef0da2124c8f7b4642f07b53a2c8c
satyam kakra <satyamkakra4u@gmail.com>
2022-05-18 19:00:54 +0530

    movie card

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -4,16 +4,29 @@ import {movies} from './MovieData'
 
 export class MovieList extends Component {
   render() {
-      let moviesArr = movies.results
-      console.log(moviesArr)
+       let moviesArr = movies.results
+    
     return (
+        <>
       <div>
+          
+          <h3 className='text-center'><strong>Trending</strong></h3>
+
+      </div>
+      <div className='movies-list'>
           {
               moviesArr.map((movieElem)=>(
-                <h1>{movieElem.title}</h1>  
+                <div className="card movie-card">
+              <img src="https://collider.com/wp-content/uploads/the-avengers-movie-poster-banners-03.jpg" style={{height:'40vh' , width:'20vw'}} className="card-img-top movie-img" alt="..."/>
+                <h5 className="card-title banner-title">{movieElem.title}</h5>
+               
+            </div>
               ))
+              
           }
+
       </div>
+      </>
     )
   }
 }
