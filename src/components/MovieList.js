0c47fe2935f5ad14f680fdfc532056be74a34db2
satyam kakra <satyamkakra4u@gmail.com>
2022-05-19 19:23:41 +0530

    next page

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { movies } from './MovieData';
+// import { movies } from './MovieData';
 import axios from 'axios'
 
 
@@ -11,22 +11,69 @@ export class MovieList extends Component {
 
     this.state = {
       hover: "",
-      parr: [1]
+      parr: [1],
+      movies: [],
+      currPage: 1
     };
   }
 
-async componentDidMount(){
-    const res = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=912d14a30748e1627dadb7802c8f3652&language=en-US&page=1`)
+  async componentDidMount() {
+    const res = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=912d14a30748e1627dadb7802c8f3652&language=en-US&page=${this.state.currPage}`)
     let movieData = res.data
     console.log(movieData)
+
+    this.setState({
+      movies: [...movieData.results]
+    })
 
     console.log('mounting done with CDM third')
   }
 
+  changeMovies = async () => {
+    const res = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=912d14a30748e1627dadb7802c8f3652&language=en-US&page=${this.state.currPage}`)
+    let movieData = res.data
+    console.log(movieData)
+
+    this.setState({
+      movies: [...movieData.results]
+    })
+
+  }
+
+  handleNext = () => {
+    let tempArr = []
+
+    for (let i = 1; i <= this.state.parr.length + 1; i++) {
+      tempArr.push(i)
+    }
+
+    this.setState({
+      parr: [...tempArr],
+      currPage: this.state.currPage + 1,
+    },
+      this.changeMovies
+    );
+  };
+
+  handlePrevious = () => {
+    if (this.state.currPage != 1) {
+      this.setState({
+        currPage: this.state.currPage - 1
+      }, this.changeMovies)
+    }
+  }
+
+  handlePageClick = (value) => {
+    if (value != this.state.currPage) {
+      this.setState({
+        currPage: value
+      }, this.changeMovies)
+    }
+  }
 
   render() {
     console.log('render second')
-    let moviesArr = movies.results;
+
 
     return (
       <>
@@ -36,7 +83,7 @@ async componentDidMount(){
 
         </div>
         <div className='movies-list'>
-          {moviesArr.map((movieElem) => (
+          {this.state.movies.map((movieElem) => (
 
             <div className="card movie-card" onMouseEnter={() => this.setState({ hover: movieElem.id })}
               onMouseLeave={() => this.setState({ hover: "" })}>
@@ -46,11 +93,11 @@ async componentDidMount(){
               <h5 className="card-title movie-title">{movieElem.title}</h5>
 
               <div className="button-wrapper" style={{ display: "flex", justifyContent: "center" }} />
-              
-              { this.state.hover == movieElem.id && (
 
-                  <a href="#" className="btn btn-primary movies-button text-center">Add to Favourites</a>
-                )}
+              {this.state.hover == movieElem.id && (
+
+                <a href="#" className="btn btn-primary movies-button text-center">Add to Favourites</a>
+              )}
               <div />
 
             </div>
@@ -60,14 +107,14 @@ async componentDidMount(){
         <div style={{ display: 'flex', justifyContent: 'center' }}>
           <nav aria-label="Page navigation example">
             <ul className="pagination">
-              <li className="page-item"><a className="page-link" href="#">Previous</a></li>
-              
-              {this.state.parr.map((value)=>(
+              <li className="page-item"><a className="page-link" onClick={this.handlePrevious}>Previous</a></li>
 
-              <li className="page-item"><a className="page-link" href="#">{value}</a></li>
+              {this.state.parr.map((value) => (
+
+                <li className="page-item"><a className="page-link" onClick={() => this.handlePageClick(value)}>{value}</a></li>
               ))}
 
-              <li className="page-item"><a className="page-link" href="#">Next</a></li>
+              <li className="page-item"><a className="page-link" onClick={this.handleNext}>Next</a></li>
             </ul>
           </nav>
         </div>
