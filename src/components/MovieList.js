0008eac9785727563ead6ccd7f24a477bd4f6fcb
satyam kakra <satyamkakra4u@gmail.com>
2022-05-19 16:13:18 +0530

    paginantion

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -3,11 +3,21 @@ import { movies } from './MovieData';
 
 
 export class MovieList extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      hover: "",
+      parr: [1]
+    };
+  }
+
   render() {
     let moviesArr = movies.results;
 
     return (
-        <>
+      <>
         <div>
 
           <h3 className='text-center'><strong>Trending</strong></h3>
@@ -15,23 +25,43 @@ export class MovieList extends Component {
         </div>
         <div className='movies-list'>
           {moviesArr.map((movieElem) => (
-            <div className="card movie-card">
+
+            <div className="card movie-card" onMouseEnter={() => this.setState({ hover: movieElem.id })}
+              onMouseLeave={() => this.setState({ hover: "" })}>
+
               <img src={`https://image.tmdb.org/t/p/original${movieElem.backdrop_path}`} style={{ height: '40vh', width: '20vw' }} className="card-img-top movie-img" alt="..." />
 
               <h5 className="card-title movie-title">{movieElem.title}</h5>
 
-              <div className="button-wrapper" style={{ display: "flex", justifyContent: "center" }}/>
-                <a href="#" className="btn btn-primary movies-button text-center">Add to Favourites</a>
-                <div />
+              <div className="button-wrapper" style={{ display: "flex", justifyContent: "center" }} />
+              
+              { this.state.hover == movieElem.id && (
 
-              </div>
-  ))}
+                  <a href="#" className="btn btn-primary movies-button text-center">Add to Favourites</a>
+                )}
+              <div />
 
             </div>
+          ))}
 
-        </>
+        </div>
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          <nav aria-label="Page navigation example">
+            <ul class="pagination">
+              <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+              
+              {this.state.parr.map((value)=>(
 
-        );
+              <li class="page-item"><a class="page-link" href="#">{value}</a></li>
+              ))}
+
+              <li class="page-item"><a class="page-link" href="#">Next</a></li>
+            </ul>
+          </nav>
+        </div>
+      </>
+
+    );
   }
 }
 
