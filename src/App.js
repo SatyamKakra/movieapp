5279cdfcfdbfc0e90ee315eb31c48e5a8b341538
satyam kakra <satyamkakra4u@gmail.com>
2022-05-20 01:01:47 +0530

    Favourites

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import './App.css';
 import Banner from './components/Banner';
 import NavBar from './components/NavBar';
 import MovieList from './components/MovieList';
+import Favourites from './components/Favourites'
+
+
 function App() {
   return (
     <>
     <NavBar/>
-    <Banner/>
-    <MovieList/>
-  
+    {/* <Banner/>
+    <MovieList/> */}
+   <Favourites/>
     </>
     
   );
